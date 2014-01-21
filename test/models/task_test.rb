06ee46a5fa6542ee81c59f7e5242f098ce12409c
FitForFuture <Serkan.Altin90@gmail.com>
2014-01-21 07:14:42 +0100

    is_delayed? Methode in Task eingefügt

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -11,5 +11,20 @@ class TaskTest < ActiveSupport::TestCase
 		task.deadline = Date.today + 7.days
 		task.duration = 5.0
 		assert task.save
-	end   
+	end  
+	test "is delayed" do
+		task = Task.new
+		task.deadline = Date.today - 10.days
+		assert task.is_delayed?
+	end
+	test "is not delayed" do
+		task = Task.new
+		task.deadline = Date.today + 10.days
+		assert !task.is_delayed?
+	end
+	test "is not delayed if deadline is today" do
+		task = Task.new
+		task.deadline = Date.today
+		assert !task.is_delayed?
+	end 
 end
