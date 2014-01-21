6bd1ed148b793cec6f5b3c7de9772c3bef787b06
Seko90 <Serkan.Altin90@gmail.com>
2014-01-21 08:42:42 +0100

    Edit und Update Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,15 @@ class ProjectsController < ApplicationController
 		@project = Project.find(params[:id])
 		@project.destroy
 	end
+	def edit
+		@project = Project.find(params[:id])
+	end
+	def update
+		@project = Project.find(params[:id])
+		if !@project.update(project_params)
+			render action: 'error'
+		end
+	end
 
 
 
